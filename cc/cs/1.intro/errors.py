1199e75c7f3dca14b902ae95ab1e6f6b80b7b7de
julius <phpninja@mail.com>
2020-10-03 16:15:14 -0700

    Create errors.py

---
+++ cc/cs/1.intro/errors.py
@@ -0,0 +1,12 @@
+
+
+# error version, mismatched single & double quote
+# print("This message has mismatched quote marks!')
+print("This message has mismatched quote marks!")
+
+# error version:
+# print("Abracadabra')
+print("Abracadabra")
+
+
+
