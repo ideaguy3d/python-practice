ef525ccdffde769f0df5d47fc4536e3288fff9bf
julius <phpninja@mail.com>
2020-10-03 16:26:40 -0700

    Create exponents.py

---
+++ cc/cs/1.intro/exponents.py
@@ -0,0 +1,13 @@
+
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6**6)
+# 7x7 quilt
+print(7**7)
+# 8x8 quilt
+print(8**8)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print((6**3)*6)
+
+
