a5050e2099ed9f94c7fed39df00ad1cba3f8b81f
julius <phpninja@mail.com>
2020-10-03 16:15:22 -0700

    Create vars.py

---
+++ cc/cs/1.intro/vars.py
@@ -0,0 +1,34 @@
+
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "lunch"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Dinner"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+
+
+
+
+
+
+
+
+
+
+
