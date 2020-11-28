3c33ddf1ca5cebba2ca42f18f631736961a1ae03
julius <phpninja@mail.com>
2020-11-27 23:24:31 -0800

    Create graph1.py

---
+++ graphs/graph1.py
@@ -0,0 +1,93 @@
+import abc
+import numpy as np
+
+
+class Graph(abc.ABC):
+    def __init__(self, num_vertices, directed=False):
+        self.num_vertices = num_vertices
+        self.directed = directed
+
+    @abc.abstractmethod
+    def add_edge(self, v1, v2, weight):
+        pass
+
+    @abc.abstractmethod
+    def get_adjacent_vertices(self, v):
+        pass
+
+    @abc.abstractmethod
+    def get_indegree(self, v):
+        pass
+
+    @abc.abstractmethod
+    def get_edge_weight(self, v1, v2):
+        pass
+
+    @abc.abstractmethod
+    def display(self):
+        pass
+
+
+class AdjacencyMatrixGraph(Graph):
+    def __init__(self, num_vertices, directed=False):
+        super(AdjacencyMatrixGraph, self).__init__(num_vertices, directed)
+
+        self.matrix = np.zeros((num_vertices, num_vertices))
+
+    def add_edge(self, v1, v2, weight=1):
+        if v1 >= self.num_vertices or v2 >= self.num_vertices or v1 < 0 or v2 < 0:
+            raise ValueError("Vert %d and %d are out of bounds" % (v1, v2))
+
+        if weight < 1:
+            raise ValueError("An edge cannot have weight < 1")
+
+        self.matrix[v1][v2] = weight
+
+        if not self.directed:
+            self.matrix[v2][v1] = weight
+
+    def get_adjacent_vertices(self, v):
+        if v < 0 or v >= self.num_vertices:
+            raise ValueError("Cannot access vert %d" % v)
+
+        adjacent_vertices = []
+        for i in range(self.num_vertices):
+            if self.matrix[v][i] > 0:
+                adjacent_vertices.append(i)
+
+        return adjacent_vertices
+
+    def get_indegree(self, v):
+        if v < 0 or v >= self.num_vertices:
+            raise ValueError("Vert %d is out of bounds" % v)
+
+    def get_edge_weight(self, v1, v2):
+        return self.matrix[v1][v2]
+
+    def display(self):
+        for i in range(self.num_vertices):
+            for v in self.get_adjacent_vertices(i):
+                print (i, "-->", v)
+
+g = AdjacencyMatrixGraph(4)
+
+g.add_edge(0,1)
+g.add_edge(0,2)
+g.add_edge(2,3)
+
+for i in range(4):
+    print("Adjacent to: ", i, g.get_adjacent_vertices(i))
+
+for i in range(4):
+    print("Indegree: ", i, g.get_indegree(i))
+
+for i in range(4):
+    for j in g.get_adjacent_vertices(i):
+        print("Edge weight: ", i, " ", j, " weight: ", g.get_edge_weight(i, j))
+
+g.display()
+
+debug = 1
+
+
+
