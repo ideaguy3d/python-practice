7f6bc53b28e49a4456a3c89419b4f47c173b6361
julius <phpninja@mail.com>
2020-12-29 17:34:54 -0800

    Create data-output.py

---
+++ pandas-fundamentals/data-output.py
@@ -0,0 +1,34 @@
+import pandas
+import os
+import sqlite3
+import sqlalchemy
+
+df = pandas.read_pickle(os.path.join('..', 'data', 'data_frame.pickle'))
+small_df = df.iloc[49980:50019, :].copy()
+path = '../data/'
+
+# basic excel
+output_excel = False
+if output_excel:
+	# ModuleNotFoundError: No module named 'openpyxl'
+	small_df.to_excel('basic.xlsx')
+	small_df.to_excel(f'{path}no_index.xlsx', index=False)
+	small_df.to_excel(f'{path}columns.xlsx', columns=['artist', 'title', 'year'])
+	# ModuleNotFoundError: No module named 'xlsxwriter'
+	writer = pandas.ExcelWriter('multiple_sheets.xlsx', engine='xlsxwriter')
+	small_df.to_excel(writer, sheet_name="Preview", index=False)
+	df.to_excel(writer, sheet_name="Complete", index=False)
+	writer.save()
+
+output_sql = False
+if output_sql:
+	# sqlalchemy.exc.ArgumentError: Could not parse rfc1738 URL from string 'mysql:dbname=hack_match;host=localhost;user=root;password=""'
+	with sqlalchemy.create_engine('mysql:dbname=hack_match;host=localhost;user=root;password=""') as conn:
+		small_df.to_sql('Tate', conn)
+
+debug = 1
+
+
+
+
+# end of file
