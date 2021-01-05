015ef2aaa795f7ff0f16a0fe5ba9dbcac81995f7
julius <phpninja@mail.com>
2021-01-04 16:19:26 -0800

    completed get_indegree()

--- coderpro/valid-binary-search-tree.py
+++ coderpro/valid-binary-search-tree.py
@@ -1,27 +1,27 @@
 class Node(object):
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+	def __init__(self, val, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
 
 
 class Solution(object):
-    def _is_valid_bst_helper(self, n, low, high):
-        if not n:
-            return True
-        val = n.val
-        if ((low < val < high) and
-                self._is_valid_bst_helper(n.left, low, n.val) and
-                self._is_valid_bst_helper(n.right, n.val, high)):
-            return True
-        return False
-
-    def is_valid_bst(self, n):
-        return self._is_valid_bst_helper(n, float('-inf'), float('inf'))
+	def _is_valid_bst_helper(self, n, low, high):
+		if not n:
+			return True
+		val = n.val
+		if ((low < val < high) and
+				self._is_valid_bst_helper(n.left, low, n.val) and
+				self._is_valid_bst_helper(n.right, n.val, high)):
+			return True
+		return False
+	
+	def is_valid_bst(self, n):
+		return self._is_valid_bst_helper(n, float('-inf'), float('inf'))
 
 
 node = Node(5)
 node.left = Node(4)
 node.right = Node(7)
-#node.right.left = Node(2)
+# node.right.left = Node(2)
 print(Solution().is_valid_bst(node))
--- graphs/graph1.py
+++ graphs/graph1.py
@@ -50,30 +50,31 @@ class AdjacencyMatrixGraph(Graph):
     def add_edge(self, v1, v2, weight=1):
         if v1 >= self.num_vertices or v2 >= self.num_vertices or v1 < 0 or v2 < 0:
             raise ValueError("Vert %d and %d are out of bounds" % (v1, v2))
-
         if weight < 1:
             raise ValueError("An edge cannot have weight < 1")
-
         self.matrix[v1][v2] = weight
-
         if not self.directed:
             self.matrix[v2][v1] = weight
+        pass
 
     def get_adjacent_vertices(self, v):
         if v < 0 or v >= self.num_vertices:
             raise ValueError("Cannot access vert %d" % v)
-
         adjacent_vertices = []
         for i in range(self.num_vertices):
             if self.matrix[v][i] > 0:
                 adjacent_vertices.append(i)
-
         return adjacent_vertices
 
     def get_indegree(self, v):
         if v < 0 or v >= self.num_vertices:
             raise ValueError("Vert %d is out of bounds" % v)
-
+        indegree = 0
+        for i in range(self.num_vertices):
+            if self.matrix[i][v] > 0:
+                indegree = indegree + 1
+        return indegree
+    
     def get_edge_weight(self, v1, v2):
         return self.matrix[v1][v2]
 
