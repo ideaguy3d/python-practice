ef6489ee4191b74790d73d1de2fd950fb1e09d57
Julius Hernandez <julius_hernandez@apple.com>
2021-04-28 12:30:04 -0700

    Update recursion.py

--- udacity/2. Data Structures/L3/recursion.py
+++ udacity/2. Data Structures/L3/recursion.py
@@ -111,6 +111,7 @@ def permutations(string):
     return return_permutations(string, 0)
 
 
+# TODO: spend more time understanding this
 def return_permutations(string, index):
     output = []
     if index >= len(string):
@@ -122,6 +123,45 @@ def return_permutations(string, index):
             new_sub_str = sub_str[0:i] + cur_char + sub_str[i:]
             output.append(new_sub_str)
     return output
+
+
+def get_characters(num):
+    if num == 2:
+        return "abc"
+    elif num == 3:
+        return "def"
+    elif num == 4:
+        return "ghi"
+    elif num == 5:
+        return "jkl"
+    elif num == 6:
+        return "mno"
+    elif num == 7:
+        return "pqrs"
+    elif num == 8:
+        return "tuv"
+    elif num == 9:
+        return "wxyz"
+    else:
+        return ""
+
+
+def keypad(num):
+    if num <= 1:
+        return [""]
+    elif 1 < num <= 9:
+        return list(get_characters(num))
+    last_digit = num % 10
+    # recursive call / call point
+    small_output = keypad(num // 10)
+    keypad_str = get_characters(last_digit)
+    output = list()
+    for char in keypad_str:
+        for item in small_output:
+            new_item = item + char
+            output.append(new_item)
+    return output
+
 
 print("\n")
 print(permutations('abc'))
