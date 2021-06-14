3dae633b393bec4b142bf47efc730cf916a8c7b0
Julius Hernandez <julius_hernandez@apple.com>
2021-06-13 23:39:04 -0700

    Update Task1.py

--- udacity/P0/Task1.py
+++ udacity/P0/Task1.py
@@ -20,3 +20,25 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+# I added a header row to the csv file
+text_header_row = texts.pop(0)
+call_header_row = calls.pop(0)
+
+text_set = set()
+call_set = set()
+
+for call in calls:
+    # add the calling & receiving numbers
+    call_set.add(call[0])
+    call_set.add(call[1])
+
+for text in texts:
+    # add the calling & receiving numbers
+    text_set.add(text[0])
+    text_set.add(text[1])
+
+total_numbers = len(text_set) + len(call_set)
+
+print(f'There are {total_numbers} different telephone numbers in the records.')
+
