74367cbfe2de743d51d0bb86eaa935ce5b01a650
Julius Hernandez <julius_hernandez@apple.com>
2021-08-31 16:22:31 -0700

    Update RANDOM_1.py

--- pandas/RANDOM_1.py
+++ pandas/RANDOM_1.py
@@ -1,13 +1,23 @@
 import pandas
 import numpy as np
 
+
+def do_op(x):
+    replace_op = False
+    if '¡' in x:
+        replace_op = True
+    if replace_op:
+        x = str(x).replace('¡', '°')
+    return x
+
 df = pandas.DataFrame({'key1': ['a', 'a', 'b', 'b', 'a'],
-                       'key2': ['one', 'two', 'one', 'two', 'one'],
+                       'key2': ['one', 'two', 'one¡', 'two', 'one'],
                        'data1': np.random.randn(5),
                        'data2': np.random.randn(5)})
 
+# simple df to practice grouping & other basic stuff
 df2 = pandas.DataFrame({'key1': ['a', 'a', 'b', 'b', 'a'],
-                        'key2': ['one', 'two', 'one', 'two', 'one'],
+                        'key2': ['one', 'two', 'one¡', 'two', 'one'],
                         'data1': [1, None, 3, 4, 5],
                         'data2': [11, 222, None, 44, 55],
                         'c3': [None, None, None, None, None]})
@@ -16,9 +26,12 @@ df2_sort1 = df2[df2.sort_values(by='data2').isnull().any(axis=1)]
 df2_sort1_ops1 = [
     df2_sort1.any(),
     df2_sort1.any(axis=1),
+    df2_sort1.isnull(),
     df2_sort1.isnull().any(),
     df2_sort1.isnull().any(axis=1)
 ]
+
+df2['key2'] = df2['key2'].apply(do_op)
 
 debug_point_01 = 1
 
