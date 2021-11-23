ace8e0e9607cab06e63a0d6afbd6749d542b597b
Julius Hernandez <julius_hernandez@apple.com>
2021-11-22 16:06:42 -0800

    simple function to test

---
+++ unit_test/Cuboid.py
@@ -0,0 +1,18 @@
+"""
+Unit Test practice
+"""
+
+
+def cuboid_volume(length):
+    if type(length) not in [int, float]:
+        raise TypeError('EXCEPTION: The length must be an int or float')
+    return length ** 3
+
+
+if __name__ == '__main__':
+    length_input = [2, 1.1, -2.5, 2j, 'two']
+
+    for i in length_input:
+        print(cuboid_volume(i))
+
+pass
